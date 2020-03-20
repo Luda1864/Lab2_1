aa44b3f11b87d55f7e054498ccded16c95bc8e08
Luda1864 <51085555+Luda1864@users.noreply.github.com>
2020-03-20 13:51:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassLibrary1.Complex t1 = new ClassLibrary1.Complex(1, 1);
+            ClassLibrary1.Complex t2 = new ClassLibrary1.Complex(2, 3), t3;
+            t3 = ClassLibrary1.Complex.sum(t1, t2);
+            Console.WriteLine(t1.ToString());
+            Console.WriteLine(t2.ToString());
+            Console.WriteLine(t3.ToString());
+        }
+    }
+}
